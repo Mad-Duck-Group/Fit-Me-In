7034e072c0d9bff4f2d5135ca94c0ca237ea38fa
Kinsagi Yuirin <sengsoon02@gmail.com>
2024-09-03 11:57:47 +0700

    Random Ver0.1

--- Assets/Scripts/Managers/RandomBlock.cs
+++ Assets/Scripts/Managers/RandomBlock.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RandomBlock : MonoBehaviour
 {
@@ -23,6 +25,7 @@ public class RandomBlock : MonoBehaviour
     private int _freeSpawnPoint;
     public int FreeSpawnPoint { get => _freeSpawnPoint; set => _freeSpawnPoint = value; }
     bool isFree = true;
+    private int i;
 
     void Awake()
     {
@@ -30,23 +33,33 @@ public class RandomBlock : MonoBehaviour
     } 
     
     // Start is called before the first frame update
-    void Update()
+    void Start()
     {
-        if (_freeSpawnPoint >= 3)
-            return;
-        SpawnRandomBlock();
+        for (i = 0; i < spawnPositions.Length; i++)
+        {
+            if (isFree == true)
+            {
+                SpawnRandomBlock();
+                if (i >= 3)
+                {
+                    isFree = false;
+                }
+            }
+        }
         
-        
+    }
+
+    private void Update()
+    {
+        Debug.Log("FreeSpawnPoint: " + _freeSpawnPoint);
+        Debug.Log("isFree: " + isFree);
     }
 
     public void SpawnRandomBlock()
     {
-        for (int i = 0; i < spawnPositions.Length; i++)
-        {
-            Transform spawnPosition = spawnPositions[i];
-            int randomIndex = Random.Range(0, randomObjects.Length);
-            Instantiate(randomObjects[randomIndex], spawnPosition.position, Quaternion.identity);
-            _freeSpawnPoint++;
-        }
+        Transform spawnPosition = spawnPositions[i];
+        int randomIndex = Random.Range(0, randomObjects.Length);
+        Instantiate(randomObjects[randomIndex], spawnPosition.position, Quaternion.identity);
+        _freeSpawnPoint++;
     }
 }
