0fcee60b04342345105c7e80d1ec3097f551f37a
MaFIaTH <mafeeyawat@gmail.com>
2024-09-06 20:43:06 +0700

    CRT Filter and Custom Cursor

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private Texture2D cursorSprite;
+
+    private void Awake()
+    {
+        Cursor.SetCursor(cursorSprite, Vector2.zero, CursorMode.Auto);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("UI");
